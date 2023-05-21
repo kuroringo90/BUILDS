701aa9f5a6d64dbc91f07b29d2d70acf927c6491
kuroringo90 <57499655+kuroringo90@users.noreply.github.com>
2023-05-21 02:58:35 +0200

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -24,40 +24,21 @@ fi
 start_time=$(date +%s)
 
 
-# Build GApps
-# if BUILD_GAPPS_COMMAND is set, otherwise skip
-if [ -n "$BUILD_GAPPS_COMMAND" ]; then
-    start_time_gapps=$(date +%s)
-    gapps_log_file="gapps_build.log"
-    logt "Building GApps..."
-    # if LOG_OUTPUT is set to false, then don't log output
-    if [ "$LOG_OUTPUT" == "false" ]; then
-        eval "$BUILD_GAPPS_COMMAND"
-        if [ $? -ne 0 ]; then
-            logt "GApps build failed. Aborting."
-            exit 1
-        fi
-    else
-        eval "$BUILD_GAPPS_COMMAND" | tee "$gapps_log_file"
-        if [ $? -ne 0 ]; then
-            logt "GApps build failed. Aborting."
-            telegram_send_file "$gapps_log_file" "GApps build log"
-            exit 1
-        fi
-        telegram_send_file "$gapps_log_file" "GApps build log"
-    fi
-    end_time_gapps=$(date +%s)
-    gapps_time_taken=$(compute_build_time "$start_time_gapps" "$end_time_gapps")
-    logt "GApps build completed in $gapps_time_taken"
-    remove_ota_package # remove OTA package if present
-    if [ $? -ne 0 ]; then
-        logt "Failed to remove OTA package. Aborting."
-        exit 1
-    fi
-else
-    echo "BUILD_GAPPS_COMMAND is not set. Skipping GApps build."
-fi
-
+# Sync source
+  if [[ "$SYNC_FLAG" == "true" ]]; then
+  logt "Syncing source..."
+  start_time_sync=$(date +%s)
+  eval "${SYNC_SOURCE_COMMAND}" | tee sync_source.log
+  if [ $? -ne 0 ]; then
+    echo "Sync failed. Aborting."
+    telegram_send_message "Sync failed. Aborting."
+    telegram_send_file sync_source.log "Sync source log"
+    exit 1
+  fi
+  end_time_sync=$(date +%s)
+  sync_time_taken=$(compute_build_time "$start_time_sync" "$end_time_sync")
+  logt "Sync completed in $sync_time_taken"
+  
 # Clean strategy
 logt "Clean Strategy..."
 # Check if CLEAN is set to "installclean"
