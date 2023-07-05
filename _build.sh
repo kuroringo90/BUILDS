1776a9423fcacf871f37e544c4ce2883a6c952df
nekomekoraiyuu <raini4645@gmail.com>
2023-07-05 12:02:07 +0600

    Perry the bashing: minor improvements
    * Add minor details to the build command
    * Added --skip-abort flag
    * Hopefully i didnt break something on this untested code

--- _build.sh
+++ _build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-
 source _utils.sh
 source ._env # remove this line if you want environment variables to be set in the shell or use a different method to set them
 
+# Parse arguments
+if [ "$#" -gt 0 ];
+	then
+		for i in $@
+		do
+			case $i in
+				--skip-abort | -sa)
+					SKIP_ABORT="TRUE"
+					shift 0
+					;;
+			esac
+		done
+fi
 # Check if required variables are set
 req_vars=("DEVICE" "ROM_NAME" "GIT_NAME" "GIT_EMAIL" "REPOS_JSON"  "SYNC_SOURCE_COMMAND" "RELEASE_GITHUB_TOKEN" "GITHUB_RELEASE_REPO" "RELEASE_OUT_DIR" "RELEASE_FILES_PATTERN")
 for var in "${req_vars[@]}"; do
@@ -14,7 +26,8 @@ done
 
 telegram_send_message "⏳"
 telegram_send_message "*Build Initiated*: [$ROM_NAME for $DEVICE]($GITHUB_RUN_URL)" true
-
+# This is very messy to read
+telegram_send_message "*Build type*: $(if [ -n "$BUILD_VANILLA_COMMAND" ] && [ -n "$BUILD_GAPPS_COMMAND" ]; then echo "GAPPS + VANILLA"; elif [ -n "$BUILD_VANILLA_COMMAND" ]; then echo "VANILLA"; elif [ -n "$BUILD_GAPPS_COMMAND" ]; then echo "GAPPS"; fi)"
 # Check either BUILD_VANILLA_COMMAND or BUILD_GAPPS_COMMAND is set
 if [ -z "$BUILD_VANILLA_COMMAND" ] && [ -z "$BUILD_GAPPS_COMMAND" ]; then
     logt "Either BUILD_VANILLA_COMMAND or BUILD_GAPPS_COMMAND is not set. Please set it in ._env"
@@ -29,7 +42,7 @@ if [[ "$SYNC_FLAG" == "true" ]]; then
     start_time_sync=$(date +%s)
     eval "${SYNC_SOURCE_COMMAND}" | tee sync_source.log
     if [ $? -ne 0 ]; then
-        echo "Sync failed. Aborting."
+        echo "[ ! ] $? | Sync failed. Aborting."
         telegram_send_message "Sync failed. Aborting."
         telegram_send_file sync_source.log "Sync source log"
         exit 1
@@ -73,14 +86,14 @@ if [ -n "$BUILD_GAPPS_COMMAND" ]; then
     eval "$BUILD_GAPPS_COMMAND"
     build_status=$?
       else
-    eval "$BUILD_GAPPS_COMMAND" | tee "$gapps_log_file"
-    build_status=${PIPESTATUS[0]}
+  		  $BUILD_GAPPS_COMMAND | tee "$gapps_log_file"
+  		  build_status=${PIPESTATUS[0]}
 fi
 
 if [ $build_status -ne 0 ]; then
-    logt "GApps build failed. Aborting."
+    logt "[ ! ] $? | GApps build failed."
     telegram_send_file "$gapps_log_file" "GApps build log"
-    exit 1
+       if [ ! "$SKIP_ABORT" == "TRUE" ]; then exit 1; fi
 fi
     end_time_gapps=$(date +%s)
     gapps_time_taken=$(compute_build_time "$start_time_gapps" "$end_time_gapps")
@@ -91,38 +104,40 @@ fi
         exit 1
     fi
 else
-    echo "BUILD_GAPPS_COMMAND is not set. Skipping GApps build."
+    echo "[ ? ] BUILD_GAPPS_COMMAND is not set. Skipping GApps build."
 fi
 
 # Build Vanilla
 # if BUILD_VANILLA_COMMAND is set, otherwise skip
 if [ -n "$BUILD_VANILLA_COMMAND" ]; then
     start_time_vanilla=$(date +%s)
+    vanilla_log_file="vanilla_build.log"
     logt "Building vanilla..."
     # if LOG_OUTPUT is set to false, then don't log output
 if [ "$LOG_OUTPUT" == "false" ]; then
-    eval "$BUILD_VANILLA_COMMAND"
+	# we
+    $BUILD_VANILLA_COMMAND
     build_status=$?
 else
-    eval "$BUILD_VANILLA_COMMAND" | tee "$vanilla_log_file"
+    $BUILD_VANILLA_COMMAND | tee "$vanilla_log_file"
     build_status=${PIPESTATUS[0]}
 fi
 
 if [ $build_status -ne 0 ]; then
-    logt "Vanilla build failed. Aborting."
+    logt "[ ! ] $? | Vanilla build failed."
     telegram_send_file "$vanilla_log_file" "Vanilla build log"
-    exit 1
+   if [ ! "$SKIP_ABORT" == "TRUE" ]; then exit 1; fi
 fi
     end_time_vanilla=$(date +%s)
     vanilla_time_taken=$(compute_build_time "$start_time_vanilla" "$end_time_vanilla")
     logt "Vanilla build completed in $vanilla_time_taken"
-    remove_ota_package # remove OTA package if present
-    if [ $? -ne 0 ]; then
+    # remove OTA package if present
+    if ! remove_ota_package; then
         logt "Failed to remove OTA package. Aborting."
         exit 1
     fi
 else
-    echo "BUILD_VANILLA_COMMAND is not set. Skipping vanilla build."
+    echo "[ ? ] BUILD_VANILLA_COMMAND is not set. Skipping vanilla build."
 fi
 
 # Release builds
