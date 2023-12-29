11ea5d7d2e732a6562afaf21352d63337c50fd7c
kuroringo90 <ak4700@gmx.com>
2023-12-29 17:34:45 +0100

    fix typo

--- _build.sh
+++ _build.sh
@@ -82,7 +82,8 @@ fi
     # If GApps build failed, exit completely
     if [ $build_status -ne 0 ]; then
         exit 1
-    fi```
+    fi
+    
     # Build Vanilla
     # if BUILD_VANILLA_COMMAND is set, otherwise skip
     if [ -n "$BUILD_VANILLA_COMMAND" ]; then
